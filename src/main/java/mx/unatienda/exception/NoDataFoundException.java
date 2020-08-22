1ef6a040b110b3952f7c3833f28fa32388820680
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-08-21 23:30:16 -0500

    Creación de la clase

--- src/main/java/mx/unatienda/exception/NoDataFoundException.java
+++ src/main/java/mx/unatienda/exception/NoDataFoundException.java
@@ -3,7 +3,33 @@ package mx.unatienda.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//Cuando no se encuentren los datos. Es un 204(Éxito)
 @ResponseStatus(code = HttpStatus.NO_CONTENT)
 public class NoDataFoundException extends RuntimeException {
 
+	public NoDataFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoDataFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoDataFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoDataFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoDataFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
 }
