75da4f9fe370d21793ce03dda06eaf37eb3aa1be
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-08-21 23:35:10 -0500

    Creación del validador

--- src/main/java/mx/unatienda/validator/ProductValidator.java
+++ src/main/java/mx/unatienda/validator/ProductValidator.java
@@ -1,5 +1,26 @@
 package mx.unatienda.validator;
 
-public class ProductValidator {
+import mx.unatienda.dto.ProductDTO;
+import mx.unatienda.entity.Product;
+import mx.unatienda.exception.ValidateServiceException;
 
+public class ProductValidator {
+	
+	public static void save(ProductDTO product) {
+		if (product.getNombre() == null || product.getNombre().trim().isEmpty()) {
+			throw new ValidateServiceException ("Necesitas escribir el nombre.");
+		}
+		 if (product.getPrecio()<0) {
+			throw new ValidateServiceException ("El precion no puede ser negativo.");
+		}
+		
+		 if (product.getNombre()== null) {
+			throw new ValidateServiceException ("El precio es requerido.");
+		}
+		// 
+		 if (product.getNombre().length()>50) {
+			throw new ValidateServiceException ("El nombre es muy largo.");
+		}
+	}  
 }
+ 
